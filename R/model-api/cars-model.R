238849e8dfda6ce36bb251a68f708ad9268ef311
James Blair <james.m.blair09@gmail.com>
2018-08-09 10:33:39 -0600

    Simplify model script

--- R/model-api/cars-model.R
+++ R/model-api/cars-model.R
@@ -1,14 +1,8 @@
 # Build simple model from mtcars dataset
 
-# Packages ----
-library(tidyverse)
-
-# Data ---
-model_data <- as_tibble(mtcars, rownames = "car_name")
-
 # Model ----
 cars_model <- lm(mpg ~ cyl + disp + hp + drat + wt + qsec + vs + am + gear + carb,
-                 data = model_data)
+                 data = mtcars)
 
 # Save model ----
-write_rds(cars_model, here::here("R", "model-api", "cars-model.rds"))
+saveRDS(cars_model, here::here("R", "model-api", "cars-model.rds"))
