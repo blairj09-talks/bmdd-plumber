c13b232df14f2c99c42454411edc2256de17d0c4
James Blair <james.m.blair09@gmail.com>
2018-11-14 22:26:59 -0700

    Update shiny app so that user data is stored and managed from the shiny app

--- R/model-input-app/app.R
+++ R/model-input-app/app.R
@@ -22,31 +22,76 @@ ui <- fluidPage(
                         choices = sort(unique(mtcars$cyl)),
                         selected = sort(unique(mtcars$cyl))[1]),
             fluidRow(
-              actionButton("submit",
-                           "Submit")
+              actionButton("add",
+                           "Add"),
+              actionButton("remove",
+                           "Remove"),
+              actionButton("predict",
+                           "Predict")
             )
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           wellPanel(
-             textOutput("raw_results")
-           )
+          tableOutput("data"),
+          wellPanel(
+            textOutput("raw_results")
+          )
         )
     )
 )
 
 server <- function(input, output) {
-  predicted_values <- eventReactive(input$submit, {
-    # Post data
-    api_res <- httr::POST(url = paste0(base_url, "/predict"),
-                           body = data.frame(hp = as.numeric(input$hp), cyl = as.numeric(input$cyl)),
-                           encode = "json")
+  # Create reactive_values
+  reactive_values <- reactiveValues(data = data.frame(),
+                                    predicted_values = NULL)
+
+  # Update user data
+  observeEvent(input$add, {
+    # Reset predicted_values
+    reactive_values$predicted_values <- NULL
     
-    httr::content(api_res, as = "text", encoding = "UTF-8")
+    # Add to data
+    data <- reactive_values$data
+    # Remove predicted column if present
+    reactive_values$data <- rbind(data[!names(data) %in% "predicted_mpg"],
+                                  data.frame(hp = as.numeric(input$hp), cyl = as.numeric(input$cyl)))
+  })
+
+  observeEvent(input$remove, {
+    # Reset predicted_values
+    reactive_values$predicted_values <- NULL
+    
+    # Set aside existing data
+    data <- reactive_values$data
+    
+    # Remove rows that match current input
+    reactive_values$data <- dplyr::anti_join(data[!names(data) %in% "predicted_mpg"],
+                                             data.frame(hp = as.numeric(input$hp), cyl = as.numeric(input$cyl)))
+  })
+
+  observeEvent(input$predict, {
+    # Make API request
+    api_res <- httr::POST(url = paste0(base_url, "/predict"),
+                          body = reactive_values$data,
+                          encode = "json")
+    
+    # Extract JSON from API response
+    reactive_values$predicted_values <- httr::content(api_res, as = "text", encoding = "UTF-8")
+    
+    # Add predicted values to data
+    reactive_values$data <- cbind(reactive_values$data, 
+                                  predicted_mpg = as.numeric(jsonlite::fromJSON(reactive_values$predicted_values)))
   })
   
-  output$raw_results <- renderPrint(predicted_values())
+  output$data <- renderTable(reactive_values$data)
+  output$raw_results <- renderPrint({
+    if (is.null(reactive_values$predicted_values)) {
+      NULL
+    } else {
+      jqr::jq(reactive_values$predicted_values)
+    }
+  })
 }
 
 shinyApp(ui = ui, server = server)
