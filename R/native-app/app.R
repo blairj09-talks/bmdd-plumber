63d907b058383e0dd046851be02be460866783c8
James Blair <james.m.blair09@gmail.com>
2019-01-10 15:32:07 -0700

    Convert native app to natively query model

--- R/native-app/app.R
+++ R/native-app/app.R
@@ -1,51 +1,54 @@
+# Native app -----------------------------------------------------------------
+
 # Shiny App for providing data input to cars plumber API
 library(shiny)
 library(httr)
 
-base_url <- config::get("base_url")
+# Load model
+cars_model <- readr::read_rds("cars-model.rds")
 
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Cars Plumber API"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("hp",
-                        "Horsepower",
-                        min = min(mtcars$hp),
-                        max = max(mtcars$hp),
-                        value = median(mtcars$hp)),
-            selectInput("cyl",
-                        "Cylinder",
-                        choices = sort(unique(mtcars$cyl)),
-                        selected = sort(unique(mtcars$cyl))[1]),
-            fluidRow(
-              actionButton("add",
-                           "Add"),
-              actionButton("remove",
-                           "Remove"),
-              actionButton("predict",
-                           "Predict")
-            )
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-          tableOutput("data"),
-          wellPanel(
-            textOutput("raw_results")
-          )
-        )
+  
+  # Application title
+  titlePanel("Cars MPG Predictor"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("hp",
+                  "Horsepower",
+                  min = min(mtcars$hp),
+                  max = max(mtcars$hp),
+                  value = median(mtcars$hp)),
+      selectInput("cyl",
+                  "Cylinder",
+                  choices = sort(unique(mtcars$cyl)),
+                  selected = sort(unique(mtcars$cyl))[1]),
+      fluidRow(
+        actionButton("add",
+                     "Add"),
+        actionButton("remove",
+                     "Remove"),
+        actionButton("predict",
+                     "Predict")
+      )
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      tableOutput("data"),
+      wellPanel(
+        textOutput("raw_results")
+      )
     )
+  )
 )
 
 server <- function(input, output) {
   # Create reactive_values
   reactive_values <- reactiveValues(data = data.frame(),
                                     predicted_values = NULL)
-
+  
   # Update user data
   observeEvent(input$add, {
     # Reset predicted_values
@@ -57,7 +60,7 @@ server <- function(input, output) {
     reactive_values$data <- rbind(data[!names(data) %in% "predicted_mpg"],
                                   data.frame(hp = as.numeric(input$hp), cyl = as.numeric(input$cyl)))
   })
-
+  
   observeEvent(input$remove, {
     # Reset predicted_values
     reactive_values$predicted_values <- NULL
@@ -69,29 +72,24 @@ server <- function(input, output) {
     reactive_values$data <- dplyr::anti_join(data[!names(data) %in% "predicted_mpg"],
                                              data.frame(hp = as.numeric(input$hp), cyl = as.numeric(input$cyl)))
   })
-
+  
   observeEvent(input$predict, {
-    # Make API request
-    api_res <- httr::POST(url = paste0(base_url, "/predict"),
-                          body = reactive_values$data,
-                          encode = "json")
-    
-    # Extract JSON from API response
-    reactive_values$predicted_values <- httr::content(api_res, as = "text", encoding = "UTF-8")
+    # Use R model to predict new values
+    reactive_values$predicted_values <- predict(cars_model, reactive_values$data)
     
     # Add predicted values to data
     if (!"predicted_mpg" %in% names(reactive_values$data)) {
       reactive_values$data <- cbind(reactive_values$data, 
-                                    predicted_mpg = as.numeric(jsonlite::fromJSON(reactive_values$predicted_values)))
+                                    predicted_mpg = reactive_values$predicted_values)
     }
   })
   
   output$data <- renderTable(reactive_values$data)
-  output$raw_results <- renderPrint({
+  output$raw_results <- renderText({
     if (is.null(reactive_values$predicted_values)) {
       "No predictions"
     } else {
-      jsonlite::prettify(reactive_values$predicted_values)
+      print(reactive_values$predicted_values)
     }
   })
 }
