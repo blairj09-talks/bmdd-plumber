13fc609bcce867aff6ea7e4e14c65b44dd0a998a
James Blair <james.m.blair09@gmail.com>
2019-01-12 17:21:41 -0700

    Update entrypoint.R to add requestBody spec

--- R/model-api/entrypoint.R
+++ R/model-api/entrypoint.R
@@ -1,4 +1,30 @@
 library(plumber)
 
-p <- plumb("plumber.R")
-p$run(port = 5762)
+pr <- plumb("plumber.R")
+
+pr$run(port = 5762,
+      swagger = function(pr, spec, ...){
+        # Define request body for POST to /predict
+        spec$paths$`/predict`$post$requestBody <- list(
+          description = "New data to predict",
+          required = TRUE,
+          content = list(
+            `application/json` = list(
+              # Define JSON schema
+              schema = list(
+                title = "Car",
+                required = c("cyl", "hp"),
+                properties = list(
+                  cyl = list(
+                    type = "integer"
+                  ),
+                  hp = list(
+                    type = "integer"
+                  )
+                )
+              )
+            )
+          )
+        )
+        spec
+      })
